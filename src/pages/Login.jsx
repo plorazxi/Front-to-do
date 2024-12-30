0bbfc71bea23a5cd301c59a853b0479c59815804
plorazxi <paulo.ss.loraschi@gmail.com>
2024-12-30 19:01:59 -0300

    Criação da pagina de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Label from "../components/label";
+import Subtitle from "../components/Subtitle";
+import InputArea from "../components/InputArea";
+import Button from "../components/Button";
+
+function Login() {
+    let [Email, setEmail] = useState('');
+    let [Senha, setSenha] = useState('');
+
+    function login() {
+        // Função para logar na API
+    }
+
+    return (
+        <InputArea>
+            <h1>To-do-list</h1>
+            <h1>Login</h1>
+            <Label type='email' name="Email" placeholder="Digite o seu email" value={Email} onChange={setEmail} />
+            <Label type='password' name="Senha" placeholder="Digite sua senha" value={Senha} onChange={setSenha} />
+            <Subtitle ask="Não tem conta?" link="Registre-se" toRouter="/register" />
+            <Button content="Realizar login" event={login}/>
+        </InputArea>
+    )
+}
+
+export default Login;
