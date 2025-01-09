f3acf478fec6c43cf1723824c5605510cfc8081a
plorazxi <paulo.ss.loraschi@gmail.com>
2025-01-08 23:32:06 -0300

    Criando estilização para quando estiver carregando os dados da API

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react';
+import { Loader } from 'lucide-react';
 
 function Button({ content, event }) {
+    let [isLoading, setisLoading] = useState(false)
+
+    async function onClick() {
+        setisLoading(true);
+        await event();
+        setisLoading(false);
+    }
+
     return (
         <div className="ContainerSubmitButton">
             <button
                 className='SubmitButton'
-                onClick={event}
+                onClick={onClick}
             >
-                {content}
+                {isLoading ? <Loader /> : content}
             </button>
         </div>
     )
